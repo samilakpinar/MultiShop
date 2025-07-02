84ea2665cbf273ee300202e0543322e12e822dd3
Şamil Akpınar <samilakpinar6@gmail.com>
2025-07-02 21:31:59 +0300

    MSP-1508 -> Category Servisinin eklenmesi

---
+++ Services/Catalog/Multishop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using Multishop.Catalog.Dtos.CategoryDtos;
+
+namespace Multishop.Catalog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        //Task ile asenkron yapı kullanılacak.
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+    }
+}
